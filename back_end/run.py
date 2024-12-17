852d5ce194e1d99235251e58f1d7d9c59c92817a
Jared Sinai Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-12-17 17:53:26 -0500

    Setup folder structure for backend
    Most files are empty for now. The team will be tasked with putting code and new files in these folders

---
+++ back_end/run.py
@@ -0,0 +1,11 @@
+
+# Create the Flask application instance here (name it app)
+
+
+#if __name__ == '__main__': ensures that the application only runs if the script 
+# is executed directly (not imported as a module elsewhere).
+if __name__ == '__main__':
+    # Run the app with the development server (useful for development)
+    # Example: app.run(debug=True)
+    pass
+
