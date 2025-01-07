d4357ed868aaff81d61283ca7ef7c26d28be7f83
posaune14 <michaelyoutz3@gmail.com>
2025-01-07 16:30:07 -0500

    .env setup for mongodb
    created and imported .env variable for db_password, included as part of mongo db set up

--- back_end/app/__init__.py
+++ back_end/app/__init__.py
@@ -1,5 +1,21 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 from .routes import init_routes
+
+load_dotenv()
+db_password = os.getenv('DB_PASSWORD')
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+uri = "mongodb+srv://root:{db_password}@cluster0.j7kdz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+client = MongoClient(uri, server_api=ServerApi('1'))
+try:
+    client.admin.command('ping')
+    print("ping")
+except Exception as e:
+    print(e)
 
 def create_app():
     app = Flask('website')
