25e1fc930b3245dcd3db1ea36563ab05e53dfc87
Jared Sinai Hernandez <48105264+jaredsina@users.noreply.github.com>
2025-01-14 12:38:25 -0500

    Setup pymongo
    Remove old unworking pymongo config from app init.
    
    Setup new config using flask_pymongo in config init
    
    Update requirements.txt with new installs

--- back_end/app/__init__.py
+++ back_end/app/__init__.py
@@ -1,26 +1,13 @@
 from flask import Flask
-from dotenv import load_dotenv
-import os
 from .routes import init_routes
-
-load_dotenv()
-db_password = os.getenv('DB_PASSWORD')
-
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-uri = "mongodb+srv://root:{db_password}@cluster0.j7kdz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-client = MongoClient(uri, server_api=ServerApi('1'))
-try:
-    client.admin.command('ping')
-    print("ping")
-except Exception as e:
-    print(e)
+from .config import init_config
 
 def create_app():
     app = Flask('website')
 
     #config 
+    init_config(app)
+
     #security key, test mode, debugging window 
 
     # Register the routes by calling init_routes
